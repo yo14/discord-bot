7f2e1c0132271940bf72a071c224bc2ab47cdacc
yo14 <ifliandry11@gmail.com>
2020-06-29 16:33:53 +0700

    Make simple event

--- bot.py
+++ bot.py
@@ -7,4 +7,13 @@ client = commands.Bot(command_prefix='.')
 async def on_ready():
     print('Bot is ready')
 
+@client.event
+async def on_member_join(member):
+    print(f'{member} has joined a server.')
+
+@client.event
+async def on_member_remove(member):
+    print(f'{member} has left a server.')
+
 client.run('your_secret_token_put_here')
+
