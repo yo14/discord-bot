212af42c97b42aad662555a00964c8ca779cbc5c
yo14 <ifliandry11@gmail.com>
2020-06-30 10:55:49 +0700

    Make unban

--- bot.py
+++ bot.py
@@ -9,13 +9,25 @@ async def on_ready():
     print('Bot is ready')
 
 @client.command()
-async def kick(ctx, member : discord.Member, *, reason=None):
-    await member.kick(reason=reason)
+async def ban(ctx, member : discord.Member, *, reason=None):
+    await member.ban(reason=reason)
+    await ctx.send(f'Banned {member.mention}')
 
 @client.command()
-async def ban(ctx, member: discord.Member, *, reason=None):
-    await member.ban(reason=reason)
+async def unban(ctx, *, member):
+    banned_users = await ctx.guild.bans()
+    member_name, member_discriminator = member.split('#')
 
-# client.run('your_secret_token_put_here')
+    for ban_entry in banned_users:
+        user = ban_entry.user
+
+        if(user.name, user.discriminator) == (member_name, member_discriminator):
+            await ctx.guild.unban(user)
+            await ctx.send(f'unbanned {user.mention}')
+            return
+
+client.run('your_secret_token_put_here')
+
+
 
 
