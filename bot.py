6ad5e701beb215ba96612692df0007509e2eb7d6
Yolla Ifliandry <36345250+yo14@users.noreply.github.com>
2020-07-01 09:34:47 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
-client.run('NzI3MDcwNDI2MDIyNTQzMzgx.Xvmf-Q.bRjTg8lUjOtWNluWpSfVEGyOZLU')
+client.run('your_secret_token_put_here')
 
 
 
